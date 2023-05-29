ac77034a9698c44bab849eb24b2e9608092f06d2
Lendo <wesreedbusiness@gmail.com>
2023-05-29 15:17:31 -0500

    Added id helper and id to new notes and delete funtion

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const uuid = require('../helpers/uuid');
 
 router.get('/',(req,res) => {
     console.log(`${req.method} request received for notes`);
@@ -19,6 +20,7 @@ router.post('/',(req,res) => {
     const newNote = {
         title,
         text,
+        id: uuid(),
     }
     //reads file of saved notes
     fs.readFile('./db/db.json', (err, data) => {
@@ -31,21 +33,38 @@ router.post('/',(req,res) => {
         JSON.stringify(parsedNotes)
         ,(err) => {
             if (err) throw err;
+            console.log("Note added");
+            res.json()
         })
 
-      });
-      
-
-
-
-
-
-
-
-    
+      });    
 })
 
+router.delete('/:id', (req, res) => {
+    console.log(`${req.method} request received for notes`);
 
 
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        
+        const parsedNotes = JSON.parse(data)
+        const { id } = req.params;
+
+        const noteIndex = parsedNotes.findIndex(note => note.id == id)
+
+        parsedNotes.splice(noteIndex, 1);
+    
+        const noteString = JSON.stringify(parsedNotes)
+
+        fs.writeFile('./db/db.json',
+        noteString
+        ,(err) => {
+            if (err) throw err;
+
+            console.log("Note deleted");
+            res.json();
+        })
+    })
+})
 
 module.exports = router;
